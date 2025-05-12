c663889d13889b1f624142d286a08d1f0c87ab7e
Félix Orain <felix.orain@gmail.com>
2025-05-12 16:02:25 +0200

    Initialized the project with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body className="flex flex-col justify-center h-full items-center">
+        <h1 className='font-bold text-3xl'>Hello</h1>
+      </body>
+    </>
+  )
+}
+
+export default App
